6cfea8ece2ecb9d2e56b976e7ee2f6dc71be0907
Brett Conti <baconti16@gmail.com>
2020-12-08 18:04:16 -0500

    add executeQuery() method

--- src/main/java/com/conti/webbible/views/home/BibleAPI.java
+++ src/main/java/com/conti/webbible/views/home/BibleAPI.java
@@ -4,6 +4,7 @@ import java.net.URLEncoder;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
@@ -15,11 +16,14 @@ public class BibleAPI {
 	private String verseNumParam;
 	private final String charset = "UTF-8";
 	private ArrayList<String> queryCache;
+	private Gson gson;
+	private String lastResultString;
 	
 	public BibleAPI() {
 		this.host = "https://bible-api.com/";
 		this.verseNumParam = "?verse_numbers=true";
 		this.queryCache = new ArrayList<String>(10);
+		this.gson = new GsonBuilder().setPrettyPrinting().create();
 	}
 	
 	private String prepareQuery(String parameters) {
@@ -36,6 +40,18 @@ public class BibleAPI {
 		}
 		
 		return this.host + formattedParams + this.verseNumParam;
+	}
+	
+	public BibleAPI executeQuery(String parameters) throws UnirestException {
+		String query = prepareQuery(parameters);
+		
+		try {
+			HttpResponse<JsonNode> response = Unirest.get(query).asJson();
+			this.lastResultString = response.getHeaders().get("text").toString();
+		}catch(UnirestException e) {
+			//do nothing
+		}
+		return this;
 	}
 	
 }
