84cfc439867f14e082352e33a1efe35825868e04
Brett Conti <baconti16@gmail.com>
2020-12-08 17:03:23 -0500

    initial setup and constructor

--- src/main/java/com/conti/webbible/views/home/BibleAPI.java
+++ src/main/java/com/conti/webbible/views/home/BibleAPI.java
@@ -1,5 +1,25 @@
 package com.conti.webbible.views.home;
 
-public class BibleAPI {
+import java.net.URLEncoder;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import java.util.ArrayList;
 
+public class BibleAPI {
+	private String host;
+	private String translationParam;
+	private final String charset = "UTF-8";
+	private ArrayList<String> queryCache;
+	
+	public BibleAPI() {
+		this.host = "https://bible-api.com/";
+		this.translationParam = "?translation=kjv";
+		this.queryCache = new ArrayList<String>();
+	}
+	
 }
